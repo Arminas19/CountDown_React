9a56d7f95d326b18bcdb1d6434b335b316112787
Arminas19 <armeska14@gmail.com>
2022-11-28 14:23:52 +0000

    started to create my days counter application, now i created a new file 'clock.jsx' to render out my time left till date.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import Clock from './clock';
+import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2022',
+            newDeadline: ''
+        }
+    }
+
+    changeDeadline() {
+        this.setState({deadline: this.state.newDeadline});
+    }
+
     render() {
         return (
-            <div> Countdown Champ, App Component </div>
+            <div className="App"> 
+                <div className='App-title'>Countdown to {this.state.deadline} </div>
+                <Clock/>
+                <div>
+                <input onChange={event => this.setState({newDeadline: event.target.value})} placeholder="new date" />
+                <button onClick={() => this.changeDeadline()}> Submit </button>
+                </div>
+            </div>
         )
     }
 }
